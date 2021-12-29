d981dbd63e2b675ee851662cd080fb8609092729
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-12-29 19:44:45 +0700

    Add files via upload

---
+++ Uprak 7 Masukkan nilai.py
@@ -0,0 +1,12 @@
+def main(n):
+    if n %3==0:
+        n**3 
+        print ("Hasilnya adalah",n**3)
+    else:
+        cek(num)
+def cek(n):
+    if n %3!=0:
+        print ("False")
+num=int(input("Masukkan nilai: "))
+main(num)
+
